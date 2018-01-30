00c05d816f3608f66f0ba8724df6923a7251d7f0
Dara Hayes <dara.hayes@redhat.com>
2018-01-30 21:02:18 +0000

    Document RecoverHandler function

--- middleware.go
+++ middleware.go
@@ -6,6 +6,10 @@ import (
 	"runtime/debug"
 )
 
+// RecoverHandler is a middleware handler function that can be used
+// to recover from unexpected panics, log a stack trace and respond with a generic
+// 500 Internal Server Error.
+// Ensures no sensitive data is leaked during panics.
 func RecoverHandler(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
